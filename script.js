889278e0bdd01112399ea0bc9bc87f02876cf601
Ana Luiza de Freitas <132159788+analufreitasx@users.noreply.github.com>
2023-08-05 23:12:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Multiplicação (a, b){
+    return a * b;
+}
+
+const num1 = Number(prompt('Digite um número inteiro: '))
+const num2 = Number(prompt('Digite outro número inteiro: '))
+alert(" O produto entre " + num1 + " e " + num2 + " é igual a " + Multiplicação(num1,num2) + ".")
